e52d4438617e3627ed2a9a1af2dcd912f63d3285
Davide Santos <davidesantos91@gmail.com>
2023-05-02 17:51:56 +0200

    Frist Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSsvTPR8qO6HecJHbZgDdq-L3TRgbB7iE",
+  authDomain: "netflix-build-clone-b4745.firebaseapp.com",
+  projectId: "netflix-build-clone-b4745",
+  storageBucket: "netflix-build-clone-b4745.appspot.com",
+  messagingSenderId: "53420781623",
+  appId: "1:53420781623:web:f3934ad186c290bd070fb2",
+};
