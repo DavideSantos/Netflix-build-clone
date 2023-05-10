8ea932393d2ed1ab67dc379db30a3aaca5e83fca
Davide Santos <davidesantos91@gmail.com>
2023-05-11 00:36:48 +0200

    Creazione navbar con logo e animazione allo scroll

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "../styles/HomeScreen.css";
+import Nav from "./Nav";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  );
 };
 
 export default HomeScreen;
