e2cedc84832f6980d3910fb4a7fd805f07cd0604
Davide Santos <davidesantos91@gmail.com>
2023-05-16 14:20:00 +0200

    creazione e implementazione delle row delle categorie

--- src/Requests.js
+++ src/Requests.js
@@ -1,5 +1,3 @@
-import instance from "./axios.js";
-
 // questa chiave di solito va inserita nel env.API_KEY
 const API_KEY = "a7be90368ac17237c1e517244e57bdae";
 
@@ -7,11 +5,12 @@ const requests = {
   fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=99`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default requests;
+// https://api.themoviedb.org/3/discover/tv?api_key=a7be90368ac17237c1e517244e57bdae&with_networks=213
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import Row from "./Row";
+import requests from "../Requests";
 
 const HomeScreen = () => {
   return (
@@ -10,7 +12,17 @@ const HomeScreen = () => {
 
       <Banner />
 
-      {/* Row */}
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movie" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
