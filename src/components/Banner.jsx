a2e2d679d9b4323e7379435bc7910e82f5e64c76
Davide Santos <davidesantos91@gmail.com>
2023-05-16 12:53:30 +0200

    aggiunto npm axios, completato banner con visualizzazione delle inforamazioni e relative fatch api su themoviedb

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Banner.css";
+import axios from "axios";
+import requests from "../Requests";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function troncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + " ..." : string;
   }
@@ -11,32 +31,20 @@ const Banner = () => {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h2 className="Banner__description">
-          {troncate(
-            `This is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description This is a test description This
-          is a test description This is a test description This is a test
-          description This is a test description`,
-            150
-          )}
+          {troncate(movie?.overview, 150)}
         </h2>
       </div>
 
