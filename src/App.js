266313e9ce2bce6117af1831d13754a4b93ca835
Davide Santos <davidesantos91@gmail.com>
2023-05-16 18:02:35 +0200

    completato login screen ed creato componente signup

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./pages/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./pages/LoginScreen";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </div>
   );
---
+++ src/components/SignupScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/SignupScreen.css";
+
+const SignupScreen = () => {
+  return <div className="signupScreen">SignupScreen</div>;
+};
+
+export default SignupScreen;
