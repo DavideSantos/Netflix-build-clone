e856b90483438620cd0caf92182d11bbbe29ca37
Davide Santos <davidesantos91@gmail.com>
2023-05-16 21:46:01 +0200

    aggiunto sistema completo per la creazione del profilo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import "./App.css";
 import HomeScreen from "./pages/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./pages/LoginScreen";
+import { useEffect } from "react";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //Logged In
+        console.log(userAuth);
+      } else {
+        //Logged Out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
