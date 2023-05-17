d46881535f440d39091404eed6f86b1a71a47125
Davide Santos <davidesantos91@gmail.com>
2023-05-17 10:46:02 +0200

    creazione ed funzione login e logout

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./pages/LoginScreen";
 import { useEffect } from "react";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //Logged In
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         //Logged Out
+        dispatch(logout);
       }
     });
 
@@ -29,6 +38,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
